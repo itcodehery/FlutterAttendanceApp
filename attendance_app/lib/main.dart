762bddbc6183e73e3f56206534c97c63afe1471c
Hari <haririo321@gmail.com>
2023-06-19 18:59:23 +0530

    Added Buttons for all Courses

--- attendance_app/lib/main.dart
+++ attendance_app/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,37 +13,21 @@ class Attendre extends StatefulWidget {
 
 class AttendreState extends State<Attendre> {
   static String printedName = "Enniki Ariyaam";
-
-  Card createNewCard(String getString) {
-    return Card(
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(5)),
-            side: BorderSide.none),
-        margin: const EdgeInsets.all(20),
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-          child: Text(getString),
-        ));
-  }
-
-  Card selectCourseCard() {
-    const String title = "Today's Classes";
-    return const Card(
-      child: SizedBox(
-        height: 60,
-        width: 200,
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(50, 20, 50, 20),
-          child: Column(
-            children: [
-              Text(title),
-            ],
-          ),
-        ),
-      ),
+  String titleText = " ";
+  String subtitleText = " ";
+  final List<String> coursesAvailable = [
+    'Piano',
+    'Keyboard',
+    'Guitar',
+    'Vocals',
+  ];
+  Widget createListTile(String en) {
+    return ListTile(
+      title: Text(en),
+      trailing: const Icon(Icons.add),
+      onTap: () {},
     );
   }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -70,14 +52,15 @@ class AttendreState extends State<Attendre> {
             backgroundColor: Colors.red[300],
             icon: const Icon(Icons.add_box_outlined),
           ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              createNewCard(printedName),
-              createNewCard("Hi"),
-              selectCourseCard(),
-            ],
+          body:ListView.builder(
+            itemCount: coursesAvailable.length,
+            itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+            title: Text(coursesAvailable.elementAt(index)),
+            trailing: const Icon(Icons.add),
+            onTap: () {},
+            );
+            }
           )),
     );
   }
