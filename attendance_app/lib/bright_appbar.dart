eec1dfa5e5327ae77d16a329cfed996ebe689ff6
Hari Prasad <haririo321@gmail.com>
2023-07-22 20:44:07 +0530

    Added new home screen with lot more details

--- attendance_app/lib/bright_appbar.dart
+++ attendance_app/lib/bright_appbar.dart
@@ -8,47 +8,11 @@ class BrightMusicAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      backgroundColor: const Color.fromARGB(255, 20, 20, 20),
       title: const Text(
         'Bright Music Academy',
-        style: TextStyle(
-          color: Color.fromARGB(255, 38, 38, 75),
-          fontFamily: 'Musicalina',
-          fontWeight: FontWeight.normal,
-          fontSize: 40,
-        ),
       ),
-      actions: <Widget>[
-        Padding(
-            padding: const EdgeInsets.only(right: 20.0, top: 0),
-            child: GestureDetector(
-              onTap: () {},
-              child: const Align(
-                alignment: Alignment.centerRight,
-                child: DecoratedBox(
-                  position: DecorationPosition.foreground,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      border: Border.symmetric(
-                          horizontal: BorderSide(
-                              color: Color.fromARGB(255, 38, 38, 75), width: 2),
-                          vertical: BorderSide(
-                              color: Color.fromARGB(255, 38, 38, 75),
-                              width: 2))),
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(14, 6, 14, 6),
-                    child: Text("SIGN IN",
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 38, 38, 75),
-                            fontSize: 14,
-                            fontFamily: 'SFProDisplay')),
-                  ),
-                ),
-              ),
-            )),
-      ],
-      centerTitle: false,
-      backgroundColor: Color(0xFFE57373),
+      centerTitle: true,
     );
   }
 }
